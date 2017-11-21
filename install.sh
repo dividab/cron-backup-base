ff3bc884a6870a86f06ad3f5486d4c5ed2a6ec16
Jonas Kello <jonas.kello@divid.se>
2017-11-21 16:19:47 +0100

    Add shebang

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
+
+# Exit if a command fails
+set -e
+
 # Install curl
 apk --no-cache add curl
 
 # Install go-cron
-curl -sSL --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
