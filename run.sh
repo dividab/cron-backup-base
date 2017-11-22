8039984d10b35ab3c60bbce2f044f08dfc7b429c
Jonas Kello <jonas.kello@divid.se>
2017-11-22 15:09:45 +0100

    No quotes

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ set -x
 if [ "${SCHEDULE}" = "**None**" ]; then
   sh backup.sh
 else
-  exec go-cron -s "\"$SCHEDULE\"" -p $HEALTH_CHECK_PORT -- /bin/sh backup.sh
+  exec go-cron -s $SCHEDULE -p $HEALTH_CHECK_PORT -- /bin/sh backup.sh
 fi
