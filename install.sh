3640f2ba1bd776a1e23b83615e0317dc16874987
Jonas Kello <jonas.kello@divid.se>
2017-11-22 11:15:50 +0100

    Add nfs-utils

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ set -e
 # Install curl
 apk --no-cache add curl
 
+# Install nfs
+apk --no-cache add nfs-utils
+
 # Install go-cron
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
